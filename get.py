c9db577ccdac161d50477232517ab35668fce117
Dev2kod <devesh4coding@gmail.com>
2025-12-22 11:34:02 +0530

    basic requests

--- get.py
+++ get.py
@@ -1,13 +1,32 @@
-from flask import Flask, request, jsonify, render_template
+import json
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/logging", methods=["GET", "POST"])
-def logging():
-    if request.method == "POST":
-        name = request.form.get("name", "Guest")
-        return f"Hello, {name}"
+@app.route('/', methods=['GET'])
+def home():
+    user_id = request.args.get('id', None)
+    age = 22
+    return json.dumps(
+        {'name': 'User1',
+         "user_id":user_id,
+         'password':"1234", 
+         "age": age}
+            )
+
+@app.route("/show",methods=["POST"])
+def vasu():
+    data = request.args.get("data","null")
+    return json.dumps({
+        "data":data
+    })
+
+@app.route("/post",methods=["GET","POST"])
+def submit():
+    data = request.form.get('data','none')
+    if request.method=="POST":
+        return json.dumps(data)
     return render_template("form.html")
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
--- post.py
+++ post.py
@@ -1,1 +1,10 @@
-from flask import Flask,url_for,redirect,render_template
+from flask import Flask,url_for,redirect,render_template, jsonify
+
+app = Flask(__name__)
+
+@app.route("/getData")
+def getData():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
